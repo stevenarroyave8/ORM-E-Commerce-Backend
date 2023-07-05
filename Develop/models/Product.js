8bdbf182f9994477b355d8f0535ef2dbf548fefb
Steven Arroyave <stevenarroyave@Stevens-MacBook-Air.local>
2023-07-05 15:19:36 -0500

    updated product columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -6,18 +6,51 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Define the Product model's columns and their data types
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    }
   },
+  // Define some options for the Product model
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
+    sequelize, 
+    timestamps: false, 
+    freezeTableName: true, 
+    underscored: true, 
     modelName: 'product',
   }
 );
 
+// Export the Product model so it can be used by other parts of the application
 module.exports = Product;
