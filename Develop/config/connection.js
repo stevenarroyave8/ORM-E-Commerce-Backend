41e7a2a16872a0b32e05df31ba0f8a352e251925
Steven Arroyave <stevenarroyave@Stevens-MacBook-Air.local>
2023-07-05 14:41:16 -0500

    updated connection file

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,5 +1,7 @@
+// Loads enviroment variables from a .env file
 require('dotenv').config();
 
+// Imports the sequilize library 
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
@@ -7,6 +9,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 8886,
       dialectOptions: {
         decimalNumbers: true,
       },
